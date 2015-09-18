2c4e1cbb78e6345b4c974d7f4f0b8c6890275d39
Eric Guo <eric.guo@sandisk.com>
2015-09-19 06:50:46 +0800

    Not relative change

--- lib/where-or.rb
+++ lib/where-or.rb
@@ -64,7 +64,7 @@ ActiveSupport.on_load(:active_record) do
     #
     # The two relations must be structurally compatible: they must be scoping the same model, and
     # they must differ only by +where+ (if no +group+ has been defined) or +having+ (if a +group+ is
-    # present). Neither relation may have a +limit+, +offset+, or +uniq+ set.
+    # present). Neither relation may have a +limit+, +offset+, or +distinct+ set.
     #
     #    Post.where("id = 1").or(Post.where("id = 2"))
     #    # SELECT `posts`.* FROM `posts`  WHERE (('id = 1' OR 'id = 2'))
